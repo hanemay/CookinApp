ab2e722881b6cee3b3b1bb8d6c40756691a245de
hanemay <hanemay@hotmail.dk>
2015-06-18 17:40:39 +0200

    Refactored CookApp
    Refactored CookApp(again :-D)
    and have made the function working.
    putted the stylesheet code on top of cookapp so it will load the styles
    before loading pictures :)

--- src/java/com/kraftfoods/ws/CookApp.java
+++ src/java/com/kraftfoods/ws/CookApp.java
@@ -41,23 +41,61 @@ public class CookApp extends HttpServlet {
        // rc = tests.recipeClassification;
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-           
             out.println("<!DOCTYPE html>");
             out.println("<html>");
+            out.println(" <style>\n" +
+                "#header {\n" +
+                "    background-color:green;\n" +
+                "    color:white;\n" +
+                "    text-align:center;\n" +
+                "    padding:5px;\n" +
+                "}\n" +
+                "#nav {\n" +
+                "    line-height:30px;\n" +
+                "    background-color:#eeeeee;\n" +
+                "    \n" +
+                "    width:100px;\n" +
+                "    float:left;\n" +
+                "    padding:5px;\n" +
+                "}\n" +
+                "#row {\n" +
+                "    display:inline-block;\n" +
+                "}\n" +
+                "#sectionl {\n" +
+                "    width:350px;\n" +
+                "    float:left;\n" +
+                "    padding:30px;\n" +
+                "}\n" +
+                "#sectionC {\n" +
+                "    width:500px;\n" +
+                "    float:center;\n" +
+                "    padding:10px;\n" +
+                "}\n" +
+                "#sectionr {\n" +
+                "    width:250px;\n" +
+                "    float:right;\n" +
+                "    padding:10px;\n" +
+                "}\n" +
+                "\n" +
+                "#footer {\n" +
+                "    background-color:green;\n" +
+                "    color:white;\n" +
+                "    clear:both;\n" +
+                "    text-align:center;\n" +
+                "    padding:5px;\n" +
+                "}\n" +
+                "</style> ");
             out.println("<head>" );
             out.println("<LINK href=\"C:/Users/hanemay/Documents/NetBeansProjects/CookingApp/web/style.css\" rel=\"stylesheet\" type=\"text/css\">");
-
-            //out.println("<title>Servlet CookApp</title>" );
             out.println("<div id=\"header\">\n" +
-"<h1>Kraft Recipes</h1>\n" +
-"</div>");
+                "<h1>Kraft Recipes</h1>\n" +
+                "</div>");
             out.println("</head>");
             out.println("<body>");
-                              Enumeration<String> infomaterials= request.getParameterNames();
-while(infomaterials.hasMoreElements()) {
-   System.out.println(infomaterials.nextElement()); 
-}
-                
+            Enumeration<String> infomaterials= request.getParameterNames();
+            while(infomaterials.hasMoreElements()) {
+                System.out.println(infomaterials.nextElement()); 
+            }    
             int amount = 100;
             String[] test = recResp.returnCats();
             try{
@@ -66,7 +104,6 @@ while(infomaterials.hasMoreElements()) {
             
             }
             Recipes reccResp = new Recipes(amount);        
-
             try{
                 if(request.getParameter("isHealthy").equalsIgnoreCase("healthy"))
                     reccResp.setHealthy(true);
@@ -80,88 +117,35 @@ while(infomaterials.hasMoreElements()) {
                     reccResp.setbIsRecipePhotoRequired(true);
                 }catch(Exception e){}
                     reccResp.setbIsRecipePhotoRequired(true);
-            while(recResp.amountOfCategories != getCount){
-                //int currentCatId = recResp.recipeCategories.getRecipeCategory().get(getCount).categoryID;
-     
-
-                       
-                
-                   reccResp.Search(recResp,recResp.recResp.getRecipeCategories().getRecipeCategory().get(getCount).categoryID);
-                 RecipeSummariesResponse  recSumResp = reccResp.results();
-                   for(int recNames = 0; recNames < reccResp.getMaxAmountItems(); recNames++) {
-                       String url = recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).photoURL;
-                       if(recNames == 0){
-                                      out.println("<div id=\"header\">\n" +
-"<h2>"+test[getCount]+"</h2>\n" +
-"</div>");}
-                       if(recNames % 2==0)
-                           out.println("<div \"row\">\n" +
-"<div id=\"sectionl\">\n" +
-"<img src=\""+url+"\" style=\"height:254px;width:254px\">\n" +
-"<p>\n" +
-"<p>Number of ingrediens needed for this recipe :<p> "+reccResp.recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getNumberOfIngredients()+"</p>\n" +
-"</p>");
-                       else{
-                           out.println("</div>\n" +
-"<div id=\"sectionr\">\n" +
-"<img src=\""+url+"\" style=\"height:254px;width:254px\">\n" +
-"<p>\n" +
-"<p>Number of ingrediens needed for this recipe :<p> "+reccResp.recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getNumberOfIngredients()+"</p>\n" +
-"</p>\n" +
-"</div>\n" +
-"<div>");}
-                                              
-
-                       //out.println("[RECIPE]" +  recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).recipeName+"");
-                       //out.println("<img src="+recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).photoURL + ">");
-                      // out.println("<p>Number of ingrediens needed for this recipe : "+reccResp.recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getNumberOfIngredients()+"</p>");
-                       
-                       /*RecipeDetailResponse rec = soapService.getRecipeByRecipeID(recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getRecipeID(), true, 1, 1);
-                       for(int ingredientCounter = 0; ingredientCounter < Integer.parseInt(recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getNumberOfIngredients()) -1 ; ingredientCounter++ ){
-                       out.print("<p>[INGREDIENT]: " + rec.recipeDetail.ingredientDetails.getIngredientDetail().get(ingredientCounter).ingredientName + " amount : "+ rec.recipeDetail.ingredientDetails.getIngredientDetail().get(ingredientCounter).quantityNum+"</p>");
-                       }*/
-                   }         
-                getCount ++;
-            }
-            
-            out.println("</body>");
-            out.println(" <style>\n" +
-"#header {\n" +
-"    background-color:green;\n" +
-"    color:white;\n" +
-"    text-align:center;\n" +
-"    padding:5px;\n" +
-"}\n" +
-"#nav {\n" +
-"    line-height:30px;\n" +
-"    background-color:#eeeeee;\n" +
-"    \n" +
-"    width:100px;\n" +
-"    float:left;\n" +
-"    padding:5px;\n" +
-"}\n" +
-"#row {\n" +
-"    display:inline-block;\n" +
-"}\n" +
-"#sectionl {\n" +
-"    width:250px;\n" +
-"    float:left;\n" +
-"    padding:10px;\n" +
-"}\n" +
-"#sectionr {\n" +
-"    width:250px;\n" +
-"    float:right;\n" +
-"    padding:10px;\n" +
-"}\n" +
-"\n" +
-"#footer {\n" +
-"    background-color:green;\n" +
-"    color:white;\n" +
-"    clear:both;\n" +
-"    text-align:center;\n" +
-"    padding:5px;\n" +
-"}\n" +
-"</style> ");
+                while(recResp.amountOfCategories != getCount){
+                reccResp.setbIsRecipePhotoRequired(true);
+                reccResp.Search(recResp,recResp.recResp.getRecipeCategories().getRecipeCategory().get(getCount).categoryID);
+                RecipeSummariesResponse  recSumResp = reccResp.results();
+                for(int recNames = 0; recNames < reccResp.getMaxAmountItems(); recNames++) {
+                    String url = recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).photoURL;
+                    if(recNames == 0){
+                        out.println("<div id=\"sectionC\">\n" +
+                        "<h2>"+test[getCount]+"</h2>\n" +
+                        "</div>");}
+                        // if(recNames % 2==0){
+                        out.println("<div \"row\">\n" +
+                            "<div id=\"sectionl\">\n" +
+                            "<h3>"+reccResp.recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).recipeName+"</h3>\n" +                                    
+                            "<img src=\""+url+"\" style=\"height:254px;width:254px\">\n" +
+                            "<p>\n" +
+                            "<p>Number of ingrediens needed for this recipe : "+reccResp.recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getNumberOfIngredients()+"</p>\n" +
+                            "");
+                        RecipeDetailResponse rec = reccResp.soapService.getRecipeByRecipeID(recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getRecipeID(), true, 1, 1);
+                        for(int ingredientCounter = 0; ingredientCounter < Integer.parseInt(recSumResp.getRecipeSummaries().getRecipeSummary().get(recNames).getNumberOfIngredients()); ingredientCounter++ ){
+                            out.println("<p>" + rec.recipeDetail.ingredientDetails.getIngredientDetail().get(ingredientCounter).ingredientName + " amount : "+ rec.recipeDetail.ingredientDetails.getIngredientDetail().get(ingredientCounter).quantityNum+" "+
+                                "</p>");
+                        }
+                        out.println("</p>");
+                        out.println("</div>\n" );
+                    }         
+                    getCount ++;
+                }
+            out.println("</body>");            
             out.println("</html>");
         }
     }
@@ -192,14 +176,7 @@ while(infomaterials.hasMoreElements()) {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
-                  Enumeration<String> infomaterials= request.getParameterNames();
-while(infomaterials.hasMoreElements()) {
-   System.out.println(infomaterials.nextElement()); 
-}
-           
-processRequest(request, response);
-      
+        processRequest(request, response);  
     }
 
     /**
